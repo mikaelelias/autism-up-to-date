0625ef377d23e73b2818c71a8900c3c0b2e73895
Mikael Elias <129218135+mikaelelias@users.noreply.github.com>
2023-07-26 21:49:38 -0300

    function

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-function alert() {
-    document.querySelectorAll(".sobre")
-    alert('Clicou')
+function mesg() {
+    alert('Botão clicado')
 }
+
+const botao = document.querySelector(".btn-inf-saiba-mais");
+botao.addEventListener("click", mesg);
